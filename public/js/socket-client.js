0b3ef2a93b14599986d9dc7c58ee7a82c246f728
DanteHarold <novianrelay@gmail.com>
2022-09-29 14:58:06 -0500

    initial chat

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,34 +1,51 @@
-const lblOnline = document.querySelector("#lblOnline");
-const lblOffline = document.querySelector("#lblOffline");
-const txtMensaje = document.querySelector("#txtMensaje");
-const btnEnviar = document.querySelector("#btnEnviar");
-
 const socket = io();
+
+let params = new URLSearchParams(window.location.search);
+
+if (!params.has("nombre") || !params.has("sala")) {
+  window.location = "index.html";
+  throw new Error("El nombre y Sala son necesarios");
+}
+
+let usuario = {
+  nombre: params.get("nombre"),
+  sala: params.get("sala"),
+};
 
 socket.on("connect", () => {
   console.log("Conectado - Cliente");
-  lblOffline.style.display = "none";
-  lblOnline.style.display = "";
+
+  socket.emit("entrar-chat", usuario, (res) => {
+    console.log("usuarios Conectados ", res);
+  });
 });
 
 socket.on("disconnect", () => {
   console.log("Desconectado del Servidor - Cliente");
-  lblOnline.style.display = "none";
-  lblOffline.style.display = "";
 });
-socket.on("enviar-mensaje", (payload) => {
-  console.log(payload);
-});
-btnEnviar.addEventListener("click", () => {
-  const mensaje = txtMensaje.value;
-  console.log(mensaje);
-  const payload = {
-    mensaje,
-    id: "123",
-    fecha: new Date().getTime(),
-  };
 
-  socket.emit("enviar-mensaje", payload, (id) => {
-    console.log("Desde el Server", id);
-  });
+//* Enviar Información
+// socket.emit(
+//   "crearMensaje",
+//   { usuario: "DanteHarold", mensaje: "Hola:)" },
+//   (id) => {
+//     console.log("Desde el Server", id);
+//   }
+// );
+
+//* Escuchar Información
+socket.on("crearMensaje", (mensaje) => {
+  console.log("Desde el Server", mensaje);
+});
+
+//* Escuhcar Cambios del Usuario
+//* Cuando un usuario entra o sale del chat
+socket.on("listaPersona", (personas) => {
+  console.log(personas);
+});
+
+//* Mensajes Privados
+
+socket.on("mensajePrivado", (mensaje) => {
+  console.log("Mensaje Privado : ", mensaje);
 });
--- sockets/controller.js
+++ sockets/controller.js
@@ -1,14 +1,55 @@
-const socketController = (socket) => {
-  console.log("Cliente Conectado!", socket.id);
+const { Usuarios } = require("../classes/usuarios");
+const { crearMensaje } = require("./util/utilidades");
 
-  socket.on("disconnect", () => {
-    console.log("Cliente Desconectado", socket.id);
+const usuarios = new Usuarios();
+debugger;
+const socketController = (client) => {
+  client.on("entrar-chat", (data, callback) => {
+    if (!data.nombre || !data.sala) {
+      return callback({
+        error: true,
+        mensaje: "El Nombre/Sala es Necesario",
+      });
+    }
+
+    client.join(data.sala);
+
+    usuarios.agregarPersona(client.id, data.nombre, data.sala);
+
+    client.broadcast
+      .to(data.sala)
+      .emit("listaPersona", usuarios.getPersonasPorSala(data.sala));
+
+    callback(usuarios.getPersonasPorSala(data.sala));
   });
 
-  socket.on("enviar-mensaje", (payload, callback) => {
-    const id = 123456;
-    callback(id);
-    socket.broadcast.emit("enviar-mensaje", payload);
+  client.on("crearMensaje", (data) => {
+    let persona = usuarios.getPersona(client.id);
+
+    let mensaje = crearMensaje(client.id, data.mensaje);
+    client.broadcast.to(persona.sala).emit("crearMensaje", mensaje);
+  });
+
+  client.on("disconnect", () => {
+    let personaBorrada = usuarios.borrarPersona(client.id);
+    client.broadcast
+      .to(personaBorrada.sala)
+      .emit(
+        "crearMensaje",
+        crearMensaje("Administrador", `${personaBorrada.nombre} salió`)
+      );
+    client.broadcast
+      .to(personaBorrada.sala)
+      .emit("listaPersona", usuarios.getPersonasPorSala(personaBorrada.sala));
+  });
+
+  //* Mensajes Privados
+
+  client.on("mensajePrivado", (data) => {
+    let persona = usuarios.getPersona(client.id);
+    client.broadcast
+      .to(data.para)
+      .emit("mensajePrivado", crearMensaje(persona.nombre, data.mensaje));
   });
 };
 module.exports = {
